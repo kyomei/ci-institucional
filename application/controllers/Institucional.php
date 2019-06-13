d43cd9b37a130952d6fc9a08680069c99f31e433
Rafael Jeferson <rafa.jefer@gmail.com>
2019-06-13 14:35:02 -0300

    Criado controller Institucional com 2 metodo empresa e servicos, e definido routes para os controllers e htaccess

---
+++ application/controllers/Institucional.php
@@ -0,0 +1,22 @@
+
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Institucional extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('home');
+	}
+
+	public function Empresa()
+	{
+		echo "ola";
+		//$this->load->view('empresa');
+	}
+
+	public function Servicoes()
+	{
+		$this->load->view('servicos');
+	}
+}

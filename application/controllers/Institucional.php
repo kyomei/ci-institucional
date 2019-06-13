61cbe9c5455f428462fd1573324cd2e1f5e65e31
Rafael Jeferson <rafa.jefer@gmail.com>
2019-06-13 19:27:07 -0300

    Configurando cache para as paginas

--- application/controllers/Institucional.php
+++ application/controllers/Institucional.php
@@ -4,6 +4,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Institucional extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+
+		// Configuração cache global na classe, ou seja para todos os métodos que possuem views como saída
+		$this->output->cache(1440); // Corresponde a 24 hrs até o cache ser atualizado
+	}
 	public function index()
 	{
 		$data['title'] = "LCI | Home";

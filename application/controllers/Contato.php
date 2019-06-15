babaab29cf4e88bc2f3168b6ae77764f80aff044
Rafael Jeferson <rafa.jefer@gmail.com>
2019-06-15 20:46:59 -0300

    Criado formulario de trabalhe conosco,  valida form,  anexa curriculo e envia um email

--- application/controllers/Contato.php
+++ application/controllers/Contato.php
@@ -59,10 +59,52 @@ class Contato extends CI_Controller {
 	{
 		$data['title'] = "LCI | Trabalhe Conosco";
 		$data['description'] = "Venha trabalhar conosco";
+
+		// Regras de validação
+		$this->form_validation->set_rules('nome', 'Nome', 'trim|required|min_length[3]');
+		$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+		$this->form_validation->set_rules('assunto', 'Assunto', 'trim|required|min_length[5]');
+		$this->form_validation->set_rules('mensagem', 'Mensagem', 'trim|required|min_length[30]');
+		
+		// Verifica se houve errors, e exibe em forma de lista não ordenada
+		if($this->form_validation->run() == FALSE) {
+			$data['formErrors'] = validation_errors('<li>', '</li>');
+		} else {
+			$uploadCurriculo = $this->UploadFile('curriculo');
+
+			// Verifica se houve algum erro passa para $data['formErrors']
+			if($uploadCurriculo['error']) {
+				$data['formErrors'] = $uploadCurriculo['message'];
+			} else {
+				// Monta a mensagem
+				$msg = " <span style='font-size:18px'><p><strong>Você tem uma nova mensagem:</strong><br />";
+				$msg .= "Via: <u>".base_url($_SERVER['REQUEST_URI'])."</u></p>";
+				$msg .= "<p><strong>Detalhes da Mensagem:</strong></p>";
+				$msg .= "<p><strong>Nome</strong> ".$formData['nome']."<br />";
+				$msg .= "<strong>Email</strong> ".$formData['email']."<br />";
+				$msg .= "<strong>Assunto</strong> ".$formData['assunto']."<br />";
+				$msg .= "<strong>Mensagem</strong> ".$formData['mensagem']."</p>";
+				$msg .= "<strong>Curricu</strong>";
+				$msg .= "<p><strong>Enviado em:</strong> ".date("d/m/Y")."</p>";
+				$msg .= "<p>Obrigado!</p></span>";
+
+				// Envia email para visitante e para o email do site
+				$formData = $this->input->post();
+				$emailStatusToVisitante = $this->SendEmail("site@ieatprofissionalizante.com.br", "LCI Institucional - Currículo Recebido", $formData['email'], $formData['nome'], $formData['assunto'], "Recebemos seu currículo, ele está sendo armazenado em nossa base de dados");
+				$emailStatusToAdmin = $this->SendEmail("site@ieatprofissionalizante.com.br", "Trabalhe conosco - Instituto","teste@ieatprofissionalizante.com.br" , "Trabalhe conosco novo currículo de ".$formData['nome'], "Nova mensagem através do seu site", $msg, $formData['email'], $formData['nome'], $uploadCurriculo['fileData']['full_path']);
+				
+				if(($emailStatusToVisitante) && ($emailStatusToAdmin)) {
+					$this->session->set_flashdata('success_msg', 'Contato recebido com sucesso!');
+				} else {
+					$data['formErrors'] = "Desculpe! Não foi possível enviar o seu contato. tente novamente mais tarde.";
+				}
+			}			
+		}
 		$this->load->view('trabalhe-conosco', $data);
 	}
 
-	private function SendEmail($from, $fromName, $to, $toName, $subject, $message)
+	
+	private function SendEmail($from, $fromName, $to, $toName, $subject, $message, $attach = null)
 	{
 		$this->load->library('email');
 
@@ -80,6 +122,9 @@ class Contato extends CI_Controller {
 		$this->email->from($from, $fromName);
 		$this->email->to($to, $toName);
 
+		if($attach)
+			$this->email->attach($attach);
+			
 		$this->email->subject($subject);
 		$this->email->message($message);
 
@@ -87,5 +132,35 @@ class Contato extends CI_Controller {
 			return true;
 		else
 			return false;
+	}
+
+	private function UploadFile($inputFileName)
+	{
+		$this->load->library('upload');
+
+		$path ="../curriculos";
+
+		// Define as configurações do arquivo
+		$config['upload_path'] = $path;
+		$config['allowed_types'] = 'doc|docx|pdf|zip|rar';
+		$config['max_size'] = '5120';
+		$config['encrypt_name'] = TRUE;
+
+		// Se não existir diretório crie
+		if(!is_dir($path))
+			mkdir($path, 0777, $recursive = true);
+
+		// Inicia biblioteca com config definidas
+		$this->upload->initialize($config);
+		
+		// Verifica se não foi feito upload exibe os errors
+		if(!$this->upload->do_upload($inputFileName)) {
+			$data['error'] = true;
+			$data['message'] = $this->upload->display_errors();
+		} else {
+			$data['error'] = false;
+			$data['fileData'] = $this->upload->data();
+		}
+		return $data;
 	}
 }
--- application/views/trabalhe-conosco.php
+++ application/views/trabalhe-conosco.php
@@ -1,6 +1,82 @@
-<?php $this->load->view('commons/header.php'); ?>
+<?php $this->load->view('commons/header.php');?>
 	<div class="container my-5">
-		<h1>Trabalhe conosco</>
+		<div class="page-header">
+			<h1>Trabalhe conosco</h1>
+		</div>
+		<div class="row">
+			<div class="col-md-8">
+				
+				<!-- Start .\ Mensagem de validação do formulário  -->
+				<?php if($formErrors): ?>
+					<div class="alert alert-danger alert-dismissible">
+						<button type="button" class="close" data-dismiss="alert">&times;</button>
+						<ul>
+							<?=$formErrors?>
+						</ul>
+					</div>
+				<?php else: 
+						if($this->session->flashdata('success_msg')){
+							
+						?>
+					<div class="alert alert-success alert-dismissible">
+						<button type="button" class="close" data-dismiss="alert">&times;</button>
+						<strong>Sucesso!</strong> <?=$this->session->flashdata('success_msg') ?>
+					</div>
+							<?php } ?>
+				<?php endif; ?>				
+				<!-- End .\ Mensagem de validação do formulário  -->
+
+				<!-- Start .\ Formulário de contato -->
+				<?= form_open_multipart(base_url('trabalhe-conosco'), array("method"=>"POST")); ?>
+					<div class="form-group">
+						<label for="nome">Nome</label>
+						<?= form_input(array("name"=>"nome","id"=>"nome"),set_value('nome'),  array("class"=>"form-control","required"=>"", "type"=>"text", "placeholder"=>"Nome"));?>
+					</div>
+					<div class="form-group">
+						<label for="email">Email address</label>
+						<?= form_input(array("name"=>"email", "id"=>"email"), set_value('email'), array("class"=>"form-control", "required"=>"", "type"=>"text", "placeholder"=>"Email")); ?>
+						<small id="emailHelp" class="form-text text-muted">Ex.: email@example.com</small>
+					</div>
+					<div class="form-group">
+						<label for="assunto">Telefone de Contato</label>
+						<?= form_input(array("name"=>"telefone", "id"=>"telefone"), set_value('telefone'), array("class"=>"form-control", "required"=>"", "type"=>"text", "placeholder"=>"Telefone de Contato")); ?>
+					</div>
+					<div class="form-group">
+						<label for="mensagem">Mensagem</label>
+						<?= form_textarea(array("name"=>"mensagem","id"=>"mensagem"), set_value('mensagem'), array("class"=>"form-control", "required"=>"", "type"=>"text","placeholder"=>"Mensagem")); ?>
+					</div>
+					<div class="form-group">
+						<div class="input-group mb-3">
+							<div class="input-group-prepend">
+								<span class="input-group-text" id="inputGroupFileAddon01">Currículo</span>
+							</div>
+							<div class="custom-file">
+								<?=	form_upload(array("name"=>"curriculo","id"=>"curriculo"), set_value('curriculo'), array("class"=>"custom-input-file","required"=>""));	?>
+								<label class="custom-file-label" for="curriculo">Escolher arquivo</label>
+							</div>
+						</div>
+					</div>
+					<?= form_submit(array("name"=>"Enviar", "id"=>"enviar"), "Enviar",array("class"=>"btn btn-dark float-right")); ?>
+				<?= form_close(); ?>
+				<!-- End .\ Formulário de contato -->
+			</div>
+			<div class="col-md-4">
+				<!-- Start .\ Informaçoes de contato -->
+				<h4>Telefones</h4>
+				<p>+55 99 99999-9999 | +55 88 8888-88888</p>
+				<hr />
+				<h4>E-mail</h4>
+				<p>contato@empresa.com.br</p>
+				<hr />
+				<h4>Endereço</h4>
+				<p>R. Quinze de ABCD - Praia vip, Vila lelopa - ES</p>
+				<hr />
+				<!-- End .\ Informaçoes de contato -->
+
+				<!-- maps -->
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.726640476743!2d-46.41774638502262!3d-23.542332384692273!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce64491e33284f%3A0xd3cd58a2fd18b76b!2sGuaianazes!5e0!3m2!1spt-BR!2sbr!4v1560529862266!5m2!1spt-BR!2sbr" width="100%" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>
+			</div>
+		</div>
 	</div>
 
-<?php $this->load->view('commons/footer.php'); ?>
+<?php $this->load->view('commons/footer.php');?>
